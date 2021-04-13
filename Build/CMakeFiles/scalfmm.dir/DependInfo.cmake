b9c5fe4ef66b98fc3f3184340fec4b92f0909ef6
danielmatteson216 <danielmatteson216@gmail.com>
2021-04-13 15:55:10 -0700

    Add /Build/CMakeFiles

---
+++ Build/CMakeFiles/scalfmm.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/scalfmm_2/scalfmm/src/FAlgorithmTimers.cpp" "/mnt/c/scalfmm_2/scalfmm/build/CMakeFiles/scalfmm.dir/src/FAlgorithmTimers.cpp.o"
+  "/mnt/c/scalfmm_2/scalfmm/src/FLog.cpp" "/mnt/c/scalfmm_2/scalfmm/build/CMakeFiles/scalfmm.dir/src/FLog.cpp.o"
+  "/mnt/c/scalfmm_2/scalfmm/src/FMemStats.cpp" "/mnt/c/scalfmm_2/scalfmm/build/CMakeFiles/scalfmm.dir/src/FMemStats.cpp.o"
+  "/mnt/c/scalfmm_2/scalfmm/src/FMpi.cpp" "/mnt/c/scalfmm_2/scalfmm/build/CMakeFiles/scalfmm.dir/src/FMpi.cpp.o"
+  "/mnt/c/scalfmm_2/scalfmm/src/FSignalHandler.cpp" "/mnt/c/scalfmm_2/scalfmm/build/CMakeFiles/scalfmm.dir/src/FSignalHandler.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../include"
+  "../inastemp/Src"
+  "inastemp/Src"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi/opal/mca/event/libevent2022/libevent"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include/openmpi/opal/mca/event/libevent2022/libevent/include"
+  "/usr/lib/x86_64-linux-gnu/openmpi/include"
+  "/usr/local/include/starpu/1.3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/scalfmm.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scalfmm.dir/src/FAlgorithmTimers.cpp.o"
+  "CMakeFiles/scalfmm.dir/src/FLog.cpp.o"
+  "CMakeFiles/scalfmm.dir/src/FMemStats.cpp.o"
+  "CMakeFiles/scalfmm.dir/src/FMpi.cpp.o"
+  "CMakeFiles/scalfmm.dir/src/FSignalHandler.cpp.o"
+  "libscalfmm.a"
+  "libscalfmm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scalfmm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
